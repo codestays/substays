9c38368455c3e7aaf725b0743aa9375d3ff7e1ff
Ernest Matshabe <matshabecapital@gmail.com>
2024-07-05 16:23:38 +0200

    search bar with input

--- client/pages/footer.js
+++ client/pages/footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
         <div className='pl-6 pr-6 pt-0' onClick={open ? sideBarState : null}>
             <div className='bg-none h-16 w-full flex flex-row justify-between'>
                 <div className='bg-none-500 h-full p-1 flex items-center justify-center gap-6'>
-                    <div className='bg-none-500 h-6 w-6 hover:cursor-pointer' onClick={sideBarState}>
+                    <div className='bg-none-500 mb-1 h-6 w-6 hover:cursor-pointer' onClick={sideBarState}>
                         <div className={`${searchOpen ? '' : ''} h-full w-full flex flex-col mt-0.5 relative`}>
                             <div className={`${open ? '' : ''} bg-chatwhite bg-opacity-90 h-1.5 w-full rounded-lg mb-1 mt-0.5`}></div>
                             <div className={`${open ? 'bg-none' : 'bg-chatwhite'} bg-opacity-90 h-1.5 w-full rounded-lg mb-1 ease-in-out duration-500`}></div>
@@ -31,10 +31,20 @@ const Footer = () => {
                         substays
                     </div>
                 </div>
-                <div className='bg-none-500 h-full w-full -mr-10 flex items-center justify-end'>
-                    <div className={`${searchOpen ? 'w-80 mr-1.' : 'w-0 mr-4'} bg-darkgrey h-9 mt-0.5 rounded-3xl absolute ease-in-out duration-500 flex items-center`}>
-                        <div className={`${searchOpen ? '': 'hidden'} ml-3`}>|</div>
-                        <div className={`${searchOpen ? 'text-chatwhite text-opacity-40 -ml-1': 'hidden'} text-sm`}>Search For Club</div>
+                <div className='bg-none-500 h-full w-full -mr-10 flex items-center justify-end relative'>
+                    {/* <div className={`${searchOpen ? 'w-80 mr-1' : 'w-0 mr-4'} bg-darkgrey h-9 mt-0.5 rounded-3xl absolute flex items-center duration-500`}>
+                        <div className={`${searchOpen ? 'animate-pulse': 'hidden'} ml-3 h-3.5 w-px bg-chatwhite ease-in-out`}></div>
+                        <input type="search" id="default-search" class={`${searchOpen ? 'w-80 mr-1 visible' : 'w-0 mr-4 hidden'} bg-darkgrey w-72 h-1 p-4 ps-0 text-sm visible border-darkgrey`} placeholder="Search For Club" required />
+                    </div> */}
+                    <div className='bg-none-500 h-8 w-full absolute rounded-2xl flex justify-end'>
+                        <div className={`${searchOpen ? 'w-full' : 'w-0'} bg-none-500 h-full mt-0 rounded-2xl duration-0`}>
+                            <div className={`${searchOpen ? '-mr-1' : ''} mr-3.5 bg-darkgrey h-full bg-none-500 flex rounded-2xl items-center relative`}>
+                                <div className={`${searchOpen ? '' : 'hidden'} h-5 w-1 ml-3 bg-chatwhite `}></div>
+                                <div className='bg-none-500 h-full w-72 -ml-1 relative flex items-center'>
+                                    <input type="search" placeholder="Search For Club" className={`${searchOpen ? 'ml-px w-72' : 'hidden'} w-1 -ml-5 bg-darkgrey absolute`} required></input>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='bg-none-500 h-full p-1 flex items-center justify-end gap-6'>
